b927929538978a2db53f0810f83653e32355f35f
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-06-17 11:54:50 -0700

    Fix password when null

--- bootstrap/container.php
+++ bootstrap/container.php
@@ -57,7 +57,7 @@ $container['cache'] = function ($c) {
         'scheme'   => 'tcp',
         'host'     => $config['host'],
         'port'     => $config['port'],
-        'password' => $config['password'],
+        'password' => ($config['password'] == 'NULL') ? null : $config['password'],
     ]);
     
     return new \App\Cache\RedisAdapter($client);
